fbad6c476f346ded8b80c16a9c614b0988709bc9
mohammedkhaledalsaedi <mohammad.k.2372@gmail.com>
2024-12-29 13:26:24 +0300

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -1,24 +1,40 @@
 `timescale 1ns / 1ps
 
-module alu #( parameter ALU_WIDTH = 32 ) (
-    input  logic [ALU_WIDTH - 1 : 0 ] op1, op2 ,
-    input  logic [ 3 : 0 ] alu_ctrl , //selcter 
-    output logic [ALU_WIDTH - 1 : 0 ] alu_result,
-    output logic Zero
+module alu (
+    input logic [31:0] op1, op2,
+    input logic [3:0] alu_ctrl , //selector 
+    output logic [31:0] alu_result,
+    output logic zero
     );
-    
-    always_comb begin
-    case (alu_ctrl) 
-        4'b0000 : alu_result = op1 + op2 ; //addition
-        4'b1000 : alu_result = op1 - op2 ; //subtraction
-        4'b0111 : alu_result = op1 & op2 ; //AND
-        4'b0110 : alu_result = op1 | op2 ; //OR 
-        4'b0001 : alu_result = op1 << op2; //SLL
-        4'b0010 : alu_result = op1 < op2 ; 
+
+always_comb begin
+    case (alu_ctrl)
+        4'b0000 : alu_result = op1 + op2;
+        4'b1000 : alu_result = op1 - op2;
+        4'b0001 : alu_result = op1 << op2;
+        4'b0010 : alu_result = $signed(op1) < $signed(op2);
+        4'b0011 : alu_result = $unsigned(op1) < $unsigned(op2);
+        4'b0100 : alu_result = op1 ^ op2;
+        4'b0101 : alu_result = op1 >> op2;
+        4'b1101 : alu_result = $signed(op1) >>> $signed(op2);
+        4'b0110 : alu_result = op1 | op2;
+        4'b0111 : alu_result = op1 & op2;
         
-        default : alu_result = 32'b0;
-        
-        
+        default : alu_result = 0;
     endcase
-    end
+end
+
+
+
+//zero flag
+
+always_comb begin
+    
+    if (alu_result == 0) zero = 1;
+    else zero = 0;
+
+end
+
+
+
 endmodule
